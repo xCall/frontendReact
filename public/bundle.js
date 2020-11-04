3c5dbc8b8b70f14250416a2118672cb8ccd4cf9e
xCall <carlosfaita99@gmail.com>
2020-11-03 22:24:36 -0300

    Module build failed Error: Cannot find module 'babel-core'

---
+++ public/bundle.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'babel-core'\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:636:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:562:25)\\n    at Module.require (internal/modules/cjs/loader.js:692:17)\\n    at require (/home/xcall/Desktop/GoStack/Estudos/Nivel-1/conceitos-react/frontend/node_modules/v8-compile-cache/v8-compile-cache.js:159:20)\\n    at Object.<anonymous> (/home/xcall/Desktop/GoStack/Estudos/Nivel-1/conceitos-react/frontend/node_modules/babel-loader/lib/index.js:3:13)\\n    at Module._compile (/home/xcall/Desktop/GoStack/Estudos/Nivel-1/conceitos-react/frontend/node_modules/v8-compile-cache/v8-compile-cache.js:192:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)\\n    at Module.load (internal/modules/cjs/loader.js:653:32)\\n    at tryModuleLoad (internal/modules/cjs/loader.js:593:12)\\n    at Function.Module._load (internal/modules/cjs/loader.js:585:3)\");\n\n//# sourceURL=webpack://frontend/./src/index.js?");
+/******/ })()
+;
